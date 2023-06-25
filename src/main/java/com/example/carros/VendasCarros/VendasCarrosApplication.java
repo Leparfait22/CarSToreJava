14ef4435893e0402d8e131fbf9315c89c8be470a
Leparfait <helomeranda@gmail.com>
2023-06-25 12:37:17 -0300

    fazendo as telas

---
+++ src/main/java/com/example/carros/VendasCarros/VendasCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.carros.VendasCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasCarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carros/VendasCarros/VendasCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carros.VendasCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
